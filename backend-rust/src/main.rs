2081505b2ba65155244712ddf7353d85e1456334
orlandotellez <espadachinkinno@gmail.com>
2026-07-29 18:39:46 -0600

    feat(backend-rust):: iniciar servidor básico con axum

--- backend-rust/src/main.rs
+++ backend-rust/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+const PORT: u16 = 4000;
+const HOST: &str = "0.0.0.0";
+
+#[tokio::main]
+async fn main() {
+    let route: Router = Router::new().route("/", get(|| async { "hola mundo" }));
+
+    let addr: String = format!("{}:{}", &HOST, &PORT);
+
+    let listener: TcpListener = TcpListener::bind(&addr)
+        .await
+        .unwrap_or_else(|e| panic!("error {}", e));
+
+    println!("Server on http://{}:{}", &HOST, PORT);
+
+    axum::serve(listener, route)
+        .await
+        .unwrap_or_else(|e| panic!("Error {}", e))
 }
